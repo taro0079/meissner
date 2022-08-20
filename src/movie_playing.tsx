1bc1ac3e2745e32c69bba7b40f613bc8863de257
Taro Morita <morita0079@gmail.com>
2022-08-20 12:28:55 +0900

    インデントの修正

--- src/movie_playing.tsx
+++ src/movie_playing.tsx
@@ -1,43 +1,46 @@
-import video_file from "./mov_hts-samp001.mp4"
-import { Button, VStack, HStack } from '@chakra-ui/react'
-import { useRef, useEffect } from 'react'
-import { dialogPath } from "./myjotai";
-import { useAtom } from "jotai";
+import video_file from "./mov_hts-samp001.mp4";
+import { Button, VStack, HStack } from "@chakra-ui/react";
+import { useRef, useEffect } from "react";
+// import { dialogPath } from "./myjotai";
+// import { useAtom } from "jotai";
 
 const MoviePlay = () => {
-	const [path] = useAtom(dialogPath)
-	const videoRef = useRef<HTMLVideoElement>(null);
-	useEffect(() => {
-		videoRef.current?.load();
-	})
-	const playStart = () => {
-		videoRef.current?.play();
-	}
-	const playPause = () => {
-		videoRef.current?.pause();
-	}
-	const playStop = () => {
-		videoRef.current?.pause();
-		if (videoRef.current?.currentTime) {
-			videoRef.current.currentTime = 0;
-		}
-	}
+  //   const [path] = useAtom(dialogPath);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    videoRef.current?.load();
+  });
+  const playStart = () => {
+    videoRef.current?.play();
+  };
+  const playPause = () => {
+    videoRef.current?.pause();
+  };
+  const playStop = () => {
+    videoRef.current?.pause();
+    if (videoRef.current?.currentTime) {
+      videoRef.current.currentTime = 0;
+    }
+  };
 
-	return (
-		<VStack>
-			<div>
-				<video key={path} src={video_file} width="600" ref={videoRef}>
-					<p>test</p>
-				</video>
-			</div>
-			<HStack>
-				<Button onClick={playStart}>Play</Button>
-				<Button onClick={playPause}>Pause</Button>
-				<Button onClick={playStop}>Stop</Button>
-			</HStack>
-		</VStack>
-	)
-
+  return (
+    <VStack>
+      <div>
+        {/* <video key={path} width="600" ref={videoRef}>
+          <source src={path} />
+          <p>test</p>
+        </video> */}
+        <video src={video_file}>
+          <p>test</p>
+        </video>
+      </div>
+      <HStack>
+        <Button onClick={playStart}>Play</Button>
+        <Button onClick={playPause}>Pause</Button>
+        <Button onClick={playStop}>Stop</Button>
+      </HStack>
+    </VStack>
+  );
 };
 
-export default MoviePlay
+export default MoviePlay;
