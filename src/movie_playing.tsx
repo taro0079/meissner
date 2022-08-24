e1d51137e6b0473c21d111c54352fe22bfc5fbaa
Taro Morita <morita0079@gmail.com>
2022-08-24 21:28:28 +0900

    startTimeで指定された時間から動画を再生する

--- src/movie_playing.tsx
+++ src/movie_playing.tsx
@@ -1,28 +1,19 @@
 import { Button, VStack, HStack } from "@chakra-ui/react";
 import { useRef, useEffect } from "react";
-import { dialogPath } from "./myjotai";
+import { dialogPath, startTimeAtom } from "./myjotai";
 import { useAtom } from "jotai";
 import { convertFileSrc } from "@tauri-apps/api/tauri";
 
+const parseTime = (time: string) => {
+	const splittedTime = time.split(":")
+	const hour = Number(splittedTime[0])
+	const minuites = Number(splittedTime[1])
+	const seconds = Number(splittedTime[2])
+	const allTime = hour * 3600 + minuites * 60 + seconds
+	return allTime
+}
+
 const MoviePlay = () => {
-  const [path] = useAtom(dialogPath);
-  const videoRef = useRef<HTMLVideoElement>(null);
-  useEffect(() => {
-    videoRef.current?.load();
-  });
-  const playStart = () => {
-    // TODO: startとendで指定した区間のみを再生する
-    videoRef.current?.play();
-  };
-  const playPause = () => {
-    videoRef.current?.pause();
-  };
-  const playStop = () => {
-    videoRef.current?.pause();
-    if (videoRef.current?.currentTime) {
-      videoRef.current.currentTime = 0;
-    }
-  };
 	const [path,] = useAtom(dialogPath);
 	const [startTime,] = useAtom(startTimeAtom)
 	const videoRef = useRef<HTMLVideoElement>(null);
