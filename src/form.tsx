2367c8674caf482507e54fbec592f860ca441b70
Taro Morita <morita0079@gmail.com>
2022-08-24 17:51:11 +0900

    使われていない変数を削除した

--- src/form.tsx
+++ src/form.tsx
@@ -2,22 +2,22 @@ import { FormControl, FormLabel, Input } from "@chakra-ui/react";
 import { PrimitiveAtom, useAtom } from "jotai";
 
 type prop = {
-  label: string;
-  atom: PrimitiveAtom<string>;
+	label: string;
+	atom: PrimitiveAtom<string>;
 };
 
 const Form = (props: prop) => {
-  const [_, setTime] = useAtom(props.atom);
+	const [, setTime] = useAtom(props.atom);
 
-  return (
-    <FormControl>
-      <FormLabel>{props.label}</FormLabel>
-      <Input
-        onChange={(event) => setTime(event.target.value)}
-        defaultValue={"00:00:00"}
-      />
-    </FormControl>
-  );
+	return (
+		<FormControl>
+			<FormLabel>{props.label}</FormLabel>
+			<Input
+				onChange={(event) => setTime(event.target.value)}
+				defaultValue={"00:00:00"}
+			/>
+		</FormControl>
+	);
 };
 
 export default Form;
