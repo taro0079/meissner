eb3676cf30d50cf024e30ac527a512ba7bf03034
Taro Morita <morita0079@gmail.com>
2022-08-24 21:38:34 +0900

    format

--- src/movie_playing.tsx
+++ src/movie_playing.tsx
@@ -4,12 +4,16 @@ import { dialogPath, startTimeAtom } from "./myjotai";
 import { useAtom } from "jotai";
 import { convertFileSrc } from "@tauri-apps/api/tauri";
 
+const convertToSecond = (hours: number, minutes: number, seconds: number) => {
+  return hours * 3600 + minutes * 60 + seconds;
+};
+
 const parseTime = (time: string) => {
   const splittedTime = time.split(":");
   const hour = Number(splittedTime[0]);
-  const minuites = Number(splittedTime[1]);
+  const minutes = Number(splittedTime[1]);
   const seconds = Number(splittedTime[2]);
-  const allTime = hour * 3600 + minuites * 60 + seconds;
+  const allTime = convertToSecond(hour, minutes, seconds);
   return allTime;
 };
 
@@ -36,45 +40,22 @@ const MoviePlay = () => {
       videoRef.current.currentTime = 0;
     }
   };
-	const [path,] = useAtom(dialogPath);
-	const [startTime,] = useAtom(startTimeAtom)
-	const videoRef = useRef<HTMLVideoElement>(null);
-	// useEffect(() => {
-	// 	videoRef.current?.load();
-	// });
-	const playStart = () => {
 
-		if (videoRef.current?.currentTime == null) {
-			return;
-		}
-		videoRef.current.currentTime = parseTime(startTime);
-		videoRef.current?.play();
-	};
-	const playPause = () => {
-		videoRef.current?.pause();
-	};
-	const playStop = () => {
-		videoRef.current?.pause();
-		if (videoRef.current?.currentTime) {
-			videoRef.current.currentTime = 0;
-		}
-	};
-
-	return (
-		<VStack>
-			<div>
-				<video key={convertFileSrc(path)} width="600" ref={videoRef}>
-					<source src={convertFileSrc(path)} />
-					<p>test</p>
-				</video>
-			</div>
-			<HStack>
-				<Button onClick={playStart}>Play</Button>
-				<Button onClick={playPause}>Pause</Button>
-				<Button onClick={playStop}>Stop</Button>
-			</HStack>
-		</VStack>
-	);
+  return (
+    <VStack>
+      <div>
+        <video key={convertFileSrc(path)} width="600" ref={videoRef}>
+          <source src={convertFileSrc(path)} />
+          <p>test</p>
+        </video>
+      </div>
+      <HStack>
+        <Button onClick={playStart}>Play</Button>
+        <Button onClick={playPause}>Pause</Button>
+        <Button onClick={playStop}>Stop</Button>
+      </HStack>
+    </VStack>
+  );
 };
 
 export default MoviePlay;
